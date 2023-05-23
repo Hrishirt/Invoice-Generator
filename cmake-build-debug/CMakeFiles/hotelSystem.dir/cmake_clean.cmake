1d4e6268ed258139bc59ad5fe5e4ac3805289a0c
Hrishi Shah <shrishi291002@gmail.com>
2023-05-23 02:06:29 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/hotelSystem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hotelSystem.dir/main.c.obj"
+  "hotelSystem.exe"
+  "hotelSystem.exe.manifest"
+  "hotelSystem.pdb"
+  "libhotelSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hotelSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
